5e2ee8e1788f9f010e5ab304a2658b5e355e55bc
Nat Weerawan <nat.wrw@gmail.com>
2014-08-10 16:36:40 +0700

    Remove mixed tab.

--- app/scripts/directives/canvasdrawer.js
+++ app/scripts/directives/canvasdrawer.js
@@ -9,66 +9,67 @@
 angular.module('angularCanvasApp')
   .directive('canvasDrawer', function () {
 
-	function get_cursor_position(e, canvas, ctx, cw, width, height) {
-	  var gCanvasElement = canvas;
+    function get_cursor_position(e, canvas, ctx, cw, width, height) {
+      var gCanvasElement = canvas;
 
-	  var kBoardWidth = width;
-	  var kBoardHeight= height;
+      var kBoardWidth = width;
+      var kBoardHeight= height;
 
-	  var kPieceWidth = cw;
-	  var kPieceHeight= cw;
+      var kPieceWidth = cw;
+      var kPieceHeight= cw;
 
-	  /* returns Cell with .row and .column properties */
-	  var x;
-	  var y;
-	  if (e.pageX != undefined && e.pageY != undefined) {
-	    x = e.pageX;
-	    y = e.pageY;
-	  } else {
-	    x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-	    y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-	  }
-	  x -= gCanvasElement.offsetLeft;
-	  y -= gCanvasElement.offsetTop;
-	  x = Math.min(x, kBoardWidth * kPieceWidth);
-	  y = Math.min(y, kBoardHeight * kPieceHeight);
-	  var row = Math.floor(y / kPieceWidth);
-	  var column = Math.floor(x / kPieceHeight);
-	  // var cell = new Cell(row, column);
-	  var cell = { row: row, col: column }
-	  console.log(cell);
-	  return cell;
-	}  	
+      /* returns Cell with .row and .column properties */
+      var x;
+      var y;
+      if (e.pageX != undefined && e.pageY != undefined) {
+        x = e.pageX;
+        y = e.pageY;
+      } else {
+        x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+      }
+      x -= gCanvasElement.offsetLeft;
+      y -= gCanvasElement.offsetTop;
+      x = Math.min(x, kBoardWidth * kPieceWidth);
+      y = Math.min(y, kBoardHeight * kPieceHeight);
+      var row = Math.floor(y / kPieceWidth);
+      var column = Math.floor(x / kPieceHeight);
+      // var cell = new Cell(row, column);
+      var cell = { row: row, col: column }
+      console.log(cell);
+      return cell;
+    }   
 
-	var ctrl = function ($scope, $element) {  	
-		console.log("IN CONTROLLER");
-	}
+    var ctrl = function ($scope, $element) {    
+        console.log("IN CONTROLLER");
+    }
 
     return {
       template: '<canvas></canvas>',
-	  scope: { 
-	  	amount: "=", 
-	  	width: '@',
-	  	height: '@',
-	  	row: '@',
-	  	col: '@',
-	  	square: '@',
-	  },
+      scope: { 
+        amount: "=", 
+        width: '@',
+        height: '@',
+        row: '@',
+        col: '@',
+        square: '@',
+      },
       restrict: 'E',
       controller: ctrl,
       replace: true,
       link: function postLink(scope, element, attrs) {
+        console.log(scope);
 
-      	var width = scope.width;
-      	var height = scope.height;
-      	var canvas = element[0];
+        var width = scope.width;
+        var height = scope.height;
+        var canvas = element[0];
         var ctx = element[0].getContext("2d");
-      	var cw = 10;
+        var cw = parseInt(scope.square, 10);
 
-      	$(element).click(function(e) {
-      		console.log("CLICKED", this);
-      		get_cursor_position(e, canvas, ctx, cw, width, height);
-      	})
+        $(element).click(function(e) {
+            console.log("CLICKED", this);
+            get_cursor_position(e, canvas, ctx, cw, width, height);
+        })
 
         element.css('width', scope.width);
         element.css('height', scope.height);
